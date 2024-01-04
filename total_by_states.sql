fffc51e3087ad0e5bd8a347ecb700fc2b23ce6e9
aurelpow <150176149+aurelpow@users.noreply.github.com>
2024-01-04 21:48:04 +0100

    Delete total_by_states.sql

--- total_by_states.sql
+++
@@ -1,9 +0,0 @@
--- QUERY to have the total of names by states & year : 
-CREATE VIEW total_name_states AS 	
-  SELECT year, state,gender ,
-	SUM(count) AS total_names,
-    COUNT(DISTINCT name) AS distinct_names,
-    (SELECT COUNT(DISTINCT name) FROM StateNames) AS total_distinct_names
-FROM StateNames
-GROUP BY year, state, gender
-HAVING year >= 1940;
